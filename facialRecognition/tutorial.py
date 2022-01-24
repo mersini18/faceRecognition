671f6b6e939dcb04e6872d8cf544b8db0b8e1d23
Mersini <benjaminmersini@hotmail.co.uk>
2022-01-24 15:57:39 +0000

    comments

--- facialRecognition/tutorial.py
+++ facialRecognition/tutorial.py
@@ -1,31 +1,43 @@
 import numpy as np
 import cv2
 
+# initialise the webcam 
 cap = cv2.VideoCapture(0)
 
+# import face and eye classifiers
 faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 eyeCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye.xml")
 
-
+# infinite loop to keep camera feed running
 while True:
+    # create variable frame to store the video output
     ret, frame = cap.read()
 
+    # gray variable changes the video output to greyscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # machine learning used to detect faces found in greyscale image
     faces = faceCascade.detectMultiScale(gray, 1.3, 5)
 
+    # takes co-ordinates of outline of the 3D face image
     for (x,y,w,h) in faces:
+        # creates a rectangle around detected face using rgb colour scheme
         cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0),5)
+        # defines an area of interest which is the co-ordinates of where a face is detected
         roiGray = gray[y:y+w, x:x+w]
         roiColor = frame[y:y+h, x:x+w]
+        # machine learning used to detect eyes found in greyscale image of the area of interest (face)
         eyes = eyeCascade.detectMultiScale(roiGray, 1.3, 5)
+        # co-ordinates of detected eyes
         for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roiColor, (ex,ey), (ex+ew, ey+eh), (0,255,0), 5) #this one
+            # creates a rectangle around the eyes, in a different colour to the rectagle around face
+            cv2.rectangle(roiColor, (ex,ey), (ex+ew, ey+eh), (0,255,0), 5)
 
+    # displays the frame on the screen so user can see
     cv2.imshow('Frame', frame)
-
+    # video will run infintely until 'q' is pressed 
     if cv2.waitKey(1)== ord('q'):
         break
-
+# 
 cap.release()
 cv2.destroyAllWindows()
 
